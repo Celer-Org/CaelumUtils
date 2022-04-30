9d27098b7eadcf52b55b87d47d4cf7b617af4a89
Boxi <hasanoes02@gmail.com>
2022-04-30 19:02:59 +0200

    Button Component

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const {src, dest, watch, series} = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
+const cleanCSS = require('gulp-clean-css')
 
 function buildStyles() {
     return src('caelum/**/*.scss')
         .pipe(sass())
         .pipe(purgecss({content: ['*.html']}))
+        //.pipe(cleanCSS({compatibility: 'ie8'})) // minify CSS
         .pipe(dest('css'))
 }
 
